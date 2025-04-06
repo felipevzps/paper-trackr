7816ec51b71e0587caf0521ed5bfd2b8fdd764af
felipevzps <felipe.vzps@gmail.com>
2025-04-06 13:47:52 -0300

    fix biorxiv request

--- paper-trackr/core/biorxiv_request.py
+++ paper-trackr/core/biorxiv_request.py
@@ -1,59 +1,42 @@
 import feedparser
-import yaml
 from datetime import datetime, timedelta
 
-def check_biorxiv_feeds(yaml_path="paper-trackr/config/search_queries.yml"):
-    matches = []
-    seen_links = set()
+# OBS: bioRxiv return feeds for the most recent 30 papers across subject categories, so I don't have to filter by the last 30 days: https://www.biorxiv.org/alertsrss
+def check_biorxiv_feeds(keywords, authors):
+    # get date from the last 30 days
+    #today = datetime.today()
+    #cutoff_date = today - timedelta(days=30)
 
-    # cutoff_date = last 30 days
-    cutoff_date = datetime.today() - timedelta(days=30)
+    # create string with keywords in the bioRxiv format 
+    subject = "+".join([kw.replace(" ", "_") for kw in keywords])
+    url = f"http://connect.biorxiv.org/biorxiv_xml.php?subject={subject}"
+    print(f"Parsing feed: {url}")
 
-    with open(yaml_path) as f:
-        queries = yaml.safe_load(f)
+    # parse feed
+    feed = feedparser.parse(url)
 
-    for query in queries:
-        if "bioRxiv" not in query.get("sources", []):
-            continue
+    articles = []
 
-        keywords = query.get("keywords", [])
-        authors = query.get("authors", [])
+    for entry in feed.entries:
+        #published = entry.get("published_parsed") or entry.get("updated_parsed")
+        #if not published:
+        #    continue
 
-        for keyword in keywords:
-            # change spaces with underscores (bioRxiv rules)
-            subject = keyword.replace(" ", "_")
-            url = f"http://connect.biorxiv.org/biorxiv_xml.php?subject={subject}"
+        #published_date = datetime(*published[:6])
+        #if published_date < cutoff_date:
+        #    continue
 
-            feed = feedparser.parse(url)
+        title = entry.get("title", "")
+        link = entry.get("link", "")
+        author_line = entry.get("author", "")
 
-            for entry in feed.entries:
-                # get published day
-                published = entry.get("published_parsed")
-                if not published:
-                    continue
+        author_match = not authors or any(a.lower() in author_line.lower() for a in authors)
 
-                published_date = datetime(*published[:6])
-                if published_date < cutoff_date:
-                    continue  # ignore if it was before the cutoff_date
+        if author_match:
+            articles.append({
+                "title": title,
+                "link": link,
+                "source": "bioRxiv",
+            })
 
-                title = entry.get("title", "")
-                summary = entry.get("summary", "")
-                link = entry.get("link", "")
-                author_line = entry.get("author", "") + " " + entry.get("summary", "")
-
-                if link in seen_links:
-                    continue
-
-                keyword_match = any(kw.lower() in (title + summary).lower() for kw in keywords)
-                author_match = not authors or any(a.lower() in author_line.lower() for a in authors)
-
-                if keyword_match and author_match:
-                    seen_links.add(link)
-                    matches.append({
-                        "title": title,
-                        "link": link,
-                        "source": "bioRxiv"
-                    })
-
-    return matches
-
+    return articles
